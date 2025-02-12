8dc0f398fca7fc0eccfa9c5d730cef0de7767cd4
Cristhian Selbach <crsthian.04@gmail.com>
2025-02-12 17:33:12 -0300

    video on hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,52 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useState, useEffect } from "react";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoSrcSet = () => {
+    window.innerWidth < 760
+      ? setVideoSrc(smallHeroVideo)
+      : setVideoSrc(heroVideo);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrcSet);
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  }, []);
+
+  useGSAP(() => {
+    gsap.to("#hero-text", {
+      opacity: 1,
+      delay: 1,
+      y: 0,
+    });
+  }, []);
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero-text" className="hero-title">
+          Iphone 15 Pro
+        </p>
+        <div className="md:w10/12 w-9/12">
+          <video
+            className="poineter-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default Hero;
